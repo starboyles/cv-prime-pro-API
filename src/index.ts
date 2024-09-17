4e50d2755c11453d47036ab429d831609a00c0d6
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-17 09:51:59 +0000

    setup

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
   res.send("CV Prime Pro Server");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port $`5000`");
+app.listen(6000, () => {
+  console.log("Server is running on port $`6000`");
 });
