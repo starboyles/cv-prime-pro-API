668da39bda925a8d8e0c175a6a39f65a7a4649e3
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-17 18:20:50 +0000

    better handling of kill-port script

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import express, { Request, Response } from "express";
 import { PORT } from "./secrets";
 const app = express();
 
-app.get("/", (req:Request, res:Response) => {
-  res.send("CV Prime Pro Server");
-});
+app.get('/', (req:Request, res:Response) => {
+  res.send("CV Prime Pro Server")
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
