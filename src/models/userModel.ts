c31d660da8c676239b83651da9b9f8c11a3354d0
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-23 02:18:23 +0000

    add: user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,14 +1,66 @@
-import mongoose, {Document, Schema } from 'mongoose';
+import mongoose, { Document, Schema } from "mongoose";
 
 export interface IUser extends Document {
-    email: string;
-    password?: string;
-    googleId?: string;
-    name: string;
-    profileImage?: string;
-    role: 'BASIC'|'PREMIUM';
-    cvCount: number;
-    resetToken?: string;
-    createdAt: Date;
-    updatedAt: Date;
+  email: string;
+  password?: string;
+  googleId?: string;
+  name: string;
+  profileImage?: string;
+  role: "BASIC" | "PREMIUM";
+  cvCount: number;
+  resetToken?: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
+
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: function () {
+        return !this.googleId;
+      },
+    },
+    googleId: {
+      type: String,
+      required: function () {
+        return !this.googleId;
+      },
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["BASIC", "PREMIUM"],
+      default: "BASIC",
+    },
+    cvCount: {
+      type: Number,
+      default: 0,
+    },
+    resetToken: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
