79e272485441805983485639eda093f9547da76c
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-23 11:14:20 +0000

    refactor: Update routes to include API versioning

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import userRoutes from './routes/user.routes';
 import cvRoutes from './routes/cv.routes';
 
 export const setupRoutes = (app: Application) => {
-  app.use('/auth', authRoutes);
-  app.use('/users', userRoutes);
-  app.use('/cv', cvRoutes);
+  app.use('api/v1/auth', authRoutes);
+  app.use('api/v1/users', userRoutes);
+  app.use('api/v1/cv', cvRoutes);
 };
