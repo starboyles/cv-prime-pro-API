3881965a01b4da95eb430dd852b19c21f9ed7eca
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-24 18:44:37 +0000

    refactor: Update route setup in

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import userRoutes from './routes/user.routes';
 import cvRoutes from './routes/cv.routes';
 
 export const setupRoutes = (app: Application) => {
-  app.use('api/v1/auth', authRoutes);
-  app.use('api/v1/users', userRoutes);
-  app.use('api/v1/cv', cvRoutes);
+  app.use('/api/v1/auth', authRoutes);
+  app.use('/api/v1/users', userRoutes);
+  app.use('/api/v1/cv', cvRoutes);
 };
+
+module.exports = setupRoutes;
