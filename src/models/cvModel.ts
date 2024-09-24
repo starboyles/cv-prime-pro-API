782ef41c41baf5faa0cfb767918f907464762fff
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-24 17:48:14 +0000

    refactor: Update default date values in cvModel and userModel schemas

--- src/models/cvModel.ts
+++ src/models/cvModel.ts
@@ -41,11 +41,11 @@ const cvSchema: Schema<ICV> = new mongoose.Schema(
     },
     createdAt: {
       type: Date,
-      default: Date.now,
+      default: Date.now(),
     },
     updatedAt: {
       type: Date,
-      default: Date.now,
+      default: Date.now(),
     },
   },
   {
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -53,11 +53,11 @@ const userSchema: Schema<IUser> = new mongoose.Schema(
     },
     createdAt: {
       type: Date,
-      default: Date.now,
+      default: Date.now(),
     },
     updatedAt: {
       type: Date,
-      default: Date.now,
+      default: Date.now(),
     },
   },
   {
