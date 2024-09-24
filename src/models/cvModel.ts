8a026ccb7476f498952bd78850b4b7849b527ae5
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-24 16:28:00 +0000

    refactor: Update cvModel schema

--- src/models/cvModel.ts
+++ src/models/cvModel.ts
@@ -11,44 +11,44 @@ export interface ICV extends Document {
   updatedAt: Date;
 }
 
-const cvSchema: Schema<ICV> = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-  },
-  company: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: String,
-    required: true,
-  },
-  isCurrentlyWorking: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  experience: {
-    type: [String],
-    required: true,
-  },
+const cvSchema: Schema<ICV> = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    isCurrentlyWorking: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    experience: {
+      type: [String],
+      required: true,
+    },
     createdAt: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now,
     },
     updatedAt: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now,
     },
-},
-{
-    timestamps: true
-}
+  },
+  {
+    timestamps: true,
+  }
 );
-
