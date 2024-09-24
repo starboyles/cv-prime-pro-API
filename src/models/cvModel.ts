9d9dfc7df2536e7a8d7a6d4626d4ec4f1ca4a753
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-24 15:58:31 +0000

    refactor: code style guidelines

--- src/models/cvModel.ts
+++ src/models/cvModel.ts
@@ -1,16 +1,16 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Document, Schema } from "mongoose";
 
-export interface ICV extends Document{
-    userId: mongoose.Types.ObjectId;
-    role: string;
-    company: string;
-    year: string; //year can be stored in formats like yyyy-yyy
-    isCurrentlyWorking: boolean
-    experience: string[]; //array of short experiences
-    createdAt: Date;
-    updatedAt: Date;
+export interface ICV extends Document {
+  userId: mongoose.Types.ObjectId;
+  role: string;
+  company: string;
+  year: string; //year can be stored in formats like yyyy-yyy
+  isCurrentlyWorking: boolean;
+  experience: string[]; //array of short experiences
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-    const cvSchema: Schema<ICV> = new mongoose.Schema({
-
+const cvSchema: Schema<ICV> = new mongoose.Schema({
+    
 });
