a0a348dfbe1070d9c43125c3afb4d7121d2251e6
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-27 15:33:40 +0000

    feat: Add createCV endpoint

---
+++ src/controllers/cv.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import CV from "../models/cvModel";
+
+exports.createCV = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user.id;
+    const { role, company, year, isCurrentlyWorking, experience } = req.body;
+    const newcv = await cvService.createCV(req.body);
+    res.status(201).json({
+      status: "success",
+      message: "CV created successfully",
+      data: {
+        CV,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+        status: "fail",
+        message: (err as Error).message,
+    });
+  }
+};
+
+
