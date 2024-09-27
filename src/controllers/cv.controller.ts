b6847e81f7773441371db8953a78a09e6147a76c
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-27 17:43:57 +0000

    refactor: Update createCV controller function to include user property in the request interface

--- src/controllers/cv.controller.ts
+++ src/controllers/cv.controller.ts
@@ -2,11 +2,18 @@ import { Request, Response } from "express";
 import CV from "../models/cvModel";
 import * as cvService from "../services/cv.service";
 
-exports.createCV = async (req: Request, res: Response) => {
+// Extend the Request interface to include user property
+interface CustomRequest extends Request {
+  user?: {
+    id: string;
+  };
+}
+
+exports.createCV = async (req: CustomRequest, res: Response) => {
   try {
-    const userId = req.user.id;
+    const userId = req.user?.id;
     const { role, company, year, isCurrentlyWorking, experience } = req.body;
-    const newCv = await cvService.createCV(req.body);
+    const newCv = await cvService.createCV({ ...req.body, userId });
     res.status(201).json({
       status: "success",
       message: "CV created successfully",
