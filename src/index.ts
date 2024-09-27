d6b718a747df4a05761b7bc9a1bfe1d89d7933d8
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-27 15:33:57 +0000

    Refactor route setup

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
 import cvRoutes from './routes/cv.routes';
 
+
 export const setupRoutes = (app: Application) => {
   app.use('/api/v1/auth', authRoutes);
   app.use('/api/v1/users', userRoutes);
