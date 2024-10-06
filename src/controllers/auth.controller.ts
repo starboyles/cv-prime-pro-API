acb8506e3d459de8f10ee407b83c83c2f9ae7a56
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-10-06 22:06:28 +0000

    fear: add jwt signup

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,22 +1,29 @@
 import { Request, Response } from "express";
-import bcrypt from "bcryptjs";
-import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
-import User from "../models/userModel.ts";
+import bcrypt from 'bcryptjs'
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.ts';
 
 dotenv.config({ path: ".env" });
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+
 export const signup = async (req: Request, res: Response) => {
-  try {
-    const { email, password, name } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({
-        status: "fail",
-        message: "User already exists",
-      });
+
+    try {
+        const { email, password, name,} = req.body;
+        // checking if user already exists
+        const existingUser = await User.findOne({ email})
+        if (existingUser) {
+            return res.status(400).json({
+                status: "fail",
+                message: "User already exists"
+            })
+        }
+        const hashedPassword = 
+        
+    } catch (err) {
+        
     }
-  } catch (err) {}
-};
+}
